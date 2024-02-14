d5f04a3cd2211291aca2c46ba6266af24271aaba
ljr979 <ljr979@uowmail.edu.au>
2024-02-14 17:36:36 +1100

    format with main block and add docstring

--- src/1_Optimising_generalisability/1_a_Model_2_normalising.py
+++ src/1_Optimising_generalisability/1_a_Model_2_normalising.py
@@ -1,4 +1,5 @@
-
+"""This is a little script which shows how the raw fluorescence trajectories are normalised to the max fluorescence. This occurs within the other scripts, so is not necessary to do unless you require normalised trajectories of novel data etc. or are labelling some new molecules to test a model on.
+"""
 import os
 import re
 import matplotlib.pyplot as plt
@@ -9,15 +10,17 @@ from random import sample
 from matplotlib import pyplot
 from numpy import where
 
-#This is a little script which shows how the raw fluorescence trajectories are normalised to the max fluorescence. This occurs within the other scripts, so is not necessary to do unless you require normalised trajectories of novel data etc. or are labelling some new molecules to test a model on.
-
 #folder with the file output from 'cleanup trajectories', which should be RAW fluorescence values which need y-axis to be normalised per trajectory.
 input_folder='data/0_raw_original_collated/2_mixed/'
 output_folder='Results/1_Optimising_generalisability/a_normalising/'
+#which experiment to target
 exp_num = 'Exp1'
+#read in data
 raw_trajectories=pd.read_csv(f'{input_folder}{exp_num}_cleaned_data.csv')
 raw_trajectories.drop([col for col in raw_trajectories.columns.tolist() if 'Unnamed: 0' in col], axis=1, inplace=True)
 normalised_trajectories = raw_trajectories.copy().set_index('molecule_number')
+#normalise to max fluoresscence of each molecule (i.e., normalise y axis)
 normalised_trajectories = (normalised_trajectories.T/normalised_trajectories.T.max()).T
+#save normalised data
 normalised_trajectories.to_csv(f'{output_folder}cleaned_data.csv')
 #this input can now be used in the 'prepare data for labelling' script prior to manual labelling prior to training. Or, this is done as part of the pipeline in A_training.py. 
--- src/1_Optimising_generalisability/1_b_Model_3_add_noise.py
+++ src/1_Optimising_generalisability/1_b_Model_3_add_noise.py
@@ -1,3 +1,8 @@
+"""This script adds gaussian noise to the end of trajectories so that all are the same length. This is done here for normalised y-axis trajectories, of varying lengths such that they are all 1000 frames long.This dataframe can then be used to LABEL the molecules (manual classification) and then train using it.
+
+Returns:
+    dataframe with noise added to terminal end
+"""
 import os
 import re
 import matplotlib.pyplot as plt
@@ -7,13 +12,15 @@ import seaborn as sns
 from random import sample
 from matplotlib import pyplot
 from numpy import where
-
-#This script adds gaussian noise to the end of trajectories so that all are the same length. This is done here for normalised y-axis trajectories, of varying lengths such that they are all 1000 frames long.This dataframe can then be used to LABEL the molecules (manual classification) and then train using it.
-input_folder='Results/1_Optimising_generalisability/a_normalising/'
-output_folder='Results/1_Optimising_generalisability/b_add_noise/'
-norm_traj= pd.read_csv(f'{input_folder}cleaned_data.csv')
-#here we are just trying to make all of the data we are training on has 1000 time points so that if I have longer time series, the model we have trained can operate on that shape of data
 def fill_nan(norm_traj):
+    """fill empty cols with NAn
+
+    Args:
+        norm_traj (DF): dataframe with y normalised trajectories in it
+
+    Returns:
+        df: df with Nans at the empty timepoints (which have no fluorescence corresponding to that frame)
+    """
     time_columns = [int(col) for col in norm_traj.columns.tolist() if col not in ['molecule_number']]
     if max(time_columns) != 1000:
         new_columns = [str(timepoint) for timepoint in range(max(time_columns)+1, 1000)]
@@ -21,9 +28,9 @@ def fill_nan(norm_traj):
 
 
     return norm_traj
-#now to find the last 50 intensity values and average + SD of each molecule intensity. this results in the new DF being made with both SD and mean for every single molecule, which we can use to make a normal distribution to draw from when 
+
 def fill_noise_50(data, num_vals=50):
-    """_summary_
+    """now to find the last 50 intensity values and average + SD of each molecule intensity. this results in the new DF being made with both SD and mean for every single molecule, which we can use to make a normal distribution to draw from when 
 
     Args:
         data (df): melted df with normalised trajectories filled to 1000 with NaN so all same length
@@ -42,23 +49,32 @@ def fill_noise_50(data, num_vals=50):
     filled_data=pd.concat(filled_data)
     return filled_data
 
-data=fill_nan(norm_traj)
-#melt so that longform 
-data=pd.melt(data, id_vars=['molecule_number'], value_vars=[col for col in data.columns.tolist() if col not in ['molecule_number']], var_name='time', value_name='intensity')
+if __name__ == "__main__":
 
-filled_data=fill_noise_50(data, num_vals=50)
+    input_folder='Results/1_Optimising_generalisability/a_normalising/'
+    output_folder='Results/1_Optimising_generalisability/b_add_noise/'
+    #read in data
+    norm_traj= pd.read_csv(f'{input_folder}cleaned_data.csv')
+    #here we are just trying to make all of the data we are training on has 1000 time points so that if I have longer time series, the model we have trained can operate on that shape of data
 
-#now to unmelt the dataframe and save it to csv to be imported in my training script :) 
-fresh_training_data = filled_data.set_index(['molecule_number', 'time'])['intensity'].unstack().reset_index()
-#save so you can use as input to prepare_data_for_labelling or to train your model
-fresh_training_data.to_csv(f'{output_folder}cleaned_data.csv')
+    #extend trajectories
+    data=fill_nan(norm_traj)
+    #melt so that longform 
+    data=pd.melt(data, id_vars=['molecule_number'], value_vars=[col for col in data.columns.tolist() if col not in ['molecule_number']], var_name='time', value_name='intensity')
+    #fill with noise
+    filled_data=fill_noise_50(data, num_vals=50)
 
-#now plot a few to make sure they're added and look okay 
-data_subset = sample(list(filled_data['molecule_number'].unique()), 10)
-for molecule, df in filled_data.groupby(['molecule_number']):
-    if molecule in data_subset: 
-        df['time']=df['time'].astype(int)
-        fig, ax = plt.subplots()
-        sns.lineplot(data=df.groupby(['time']).mean().reset_index(), x='time', y='intensity')
-        plt.title(f'Molecule {molecule}')
-        plt.show()
+    #now to unmelt the dataframe and save it to csv to be imported in my training script :) 
+    fresh_training_data = filled_data.set_index(['molecule_number', 'time'])['intensity'].unstack().reset_index()
+    #save so you can use as input to prepare_data_for_labelling or to train your model
+    fresh_training_data.to_csv(f'{output_folder}cleaned_data.csv')
+
+    #now plot a few to make sure they're added and look okay 
+    data_subset = sample(list(filled_data['molecule_number'].unique()), 10)
+    for molecule, df in filled_data.groupby(['molecule_number']):
+        if molecule in data_subset: 
+            df['time']=df['time'].astype(int)
+            fig, ax = plt.subplots()
+            sns.lineplot(data=df.groupby(['time']).mean().reset_index(), x='time', y='intensity')
+            plt.title(f'Molecule {molecule}')
+            plt.show()
